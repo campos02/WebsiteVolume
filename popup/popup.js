9223402bfce42d700f710cbba338f56f0765c92c
campos <nonesovile96@hotmail.com>
2024-03-31 00:32:56 -0300

    Add boost toggle

--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,11 @@
 import { setSavedGain, setGain, getCurrentTab } from "../modules/set-gain.js";
 
+const boostCheckbox = document.getElementById("boost-checkbox");
 const volumeLabel = document.getElementById("volume-label");
 const volumeSlider = document.getElementById("volume-slider");
 const volumeOutput = document.getElementById("volume");
 
-volumeOutput.textContent = volumeSlider.value + "%";
-setSavedGain().then(async (gain) => {
-    const tab = await getCurrentTab();
-    const website = new URL(tab.url).hostname;
-
-    volumeLabel.innerText = "Setting the volume for " + website;
-    volumeSlider.value = gain * 100;
-    volumeOutput.textContent = volumeSlider.value + "%";
-});
-
-volumeSlider.oninput = async () => {
+async function setSelectedGain() {
     volumeOutput.textContent = volumeSlider.value + "%";
     
     const tab = await getCurrentTab();
@@ -36,3 +27,33 @@ volumeSlider.oninput = async () => {
     websites.add(websiteInfo);
     chrome.storage.sync.set({ websites: [...websites] });
 }
+
+volumeOutput.textContent = volumeSlider.value + "%";
+setSavedGain().then(async (gain) => {
+    const tab = await getCurrentTab();
+    const website = new URL(tab.url).hostname;
+
+    volumeLabel.innerText = `Setting the volume for ${website}:`;
+
+    if (gain > 1) {
+        volumeSlider.max = 800;
+        boostCheckbox.checked = true;
+    }
+    
+    volumeSlider.value = gain * 100;
+    volumeOutput.textContent = volumeSlider.value + "%";
+});
+
+boostCheckbox.oninput = async () => {
+    if (boostCheckbox.checked)
+        volumeSlider.max = 800;
+    else {
+        volumeSlider.max = 100;
+        volumeOutput.textContent = volumeSlider.value + "%";
+
+        if (volumeSlider.value == 100)
+            await setSelectedGain();
+    }
+};
+
+volumeSlider.oninput = setSelectedGain;
