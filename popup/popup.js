870f6468a706d6bb1e95b68fdd162602e3d5d5da
campos <nonesovile96@hotmail.com>
2024-03-30 23:36:55 -0300

    Add website indicator

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,15 @@
 import { setSavedGain, setGain, getCurrentTab } from "../modules/set-gain.js";
 
+const volumeLabel = document.getElementById("volume-label");
 const volumeSlider = document.getElementById("volume-slider");
 const volumeOutput = document.getElementById("volume");
 
 volumeOutput.textContent = volumeSlider.value + "%";
-setSavedGain().then((gain) => {
+setSavedGain().then(async (gain) => {
+    const tab = await getCurrentTab();
+    const website = new URL(tab.url).hostname;
+
+    volumeLabel.innerText = "Setting the volume for " + website;
     volumeSlider.value = gain * 100;
     volumeOutput.textContent = volumeSlider.value + "%";
 });
@@ -18,7 +23,6 @@ volumeSlider.oninput = async () => {
     await setGain(tab, volumeSlider.value / 100);
 
     const websiteInfo = {
-        enabled: true,
         website: website,
         gain: volumeSlider.value / 100
     }
